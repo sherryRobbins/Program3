c0d143348287be0a5d101635723e8ee08fafe866
Dominic Santangelo <santangeloboy@gmail.com>
2020-11-22 10:45:07 -0500

    Update program3.cpp

--- program3.cpp
+++ program3.cpp
@@ -19,8 +19,10 @@ public: //attributes
 		this->transProbability = 0;
 		this->position = -1;
 		this->isObstacle = 0;
-		this->qValue[4] = { 0 };
-		this->accessFrequency[4] = { 0 };
+		for (int i = 0; i < 4; i++) {
+			this->qValue[i] = { 0 };
+			this->accessFrequency[i] = { 0 };
+		}
 	}
 };
 
@@ -39,16 +41,19 @@ void populateGrid(GridBox gridArray[6][5]);
 void displayGrid(GridBox gridArray[6][5]);
 int *motion(GridBox gridArray[6][5], char direction, int robotLocation);
 int randomNumberGen(int maxNum);//Returns the random number
-int* findRobotLocation(int robotLocation);
+int* findLocation(int robotLocation);
+void trial(int robotLocation, GridBox gridArray[6][5], int moves);
+char findAction(GridBox gridArray[6][5], int robotLocation);
+float max(float array[]);
+
 int main() {
 	Robot robot = Robot();
 	GridBox gridArray[6][5];
 	populateGrid(gridArray);
-
 	system("Pause");
-	delete gridArray;
 	return 0;
 }
+
 int randomNumberGen(int maxNum)
 {
 	int num;
@@ -56,6 +61,7 @@ int randomNumberGen(int maxNum)
 	num = rand() % (maxNum + 1);
 	return num;
 }
+
 void populateGrid(GridBox gridArray[6][5]) {
 	int currentPosition = 1;
 	for (int i = 0; i < 6; i++) {
@@ -98,14 +104,18 @@ void displayGrid(GridBox gridArray[6][5]) {
 	gridArray: the array that stores values of squares
 	direction: the direction the robot is moving in
 	*/
-int *motion(GridBox gridArray[6][5], char direction, int robotLocation) {
-	int val;
+int *motion(GridBox gridArray[6][5], char action, int state) {
+	int tuple[5];
+	tuple[0] = state; //'state' value in the tuple
+	int val, directionInt, nextActionInt;
+	char direction, nextAction;
 	char actualDirection[3];
 	//This switch statement finds if we end up drifting or not
-	switch (direction) {
+	switch (action) {
 	case 'W':
 		actualDirection[0] = 15; actualDirection[1] = 30; actualDirection[2] = 100;
 		val = randomNumberGen(100);
+		directionInt = 1; //for tuple
 		if (val <= 15) //0 is north, 1 is south, 2 is west
 			direction = 'N';
 		else if (val > 15 && val <= 30)
@@ -116,6 +126,7 @@ int *motion(GridBox gridArray[6][5], char direction, int robotLocation) {
 	case 'N':
 		actualDirection[0] = 15; actualDirection[1] = 30; actualDirection[2] = 100;
 		val = randomNumberGen(100);
+		directionInt = 2;
 		if (val <= 15) //0 is west, 1 is east, 2 is north
 			direction = 'W';
 		else if (val > 15 && val <= 30)
@@ -126,6 +137,7 @@ int *motion(GridBox gridArray[6][5], char direction, int robotLocation) {
 	case 'E':
 		actualDirection[0] = 15; actualDirection[1] = 30; actualDirection[2] = 100;
 		val = randomNumberGen(100);
+		directionInt = 3;
 		if (val <= 15)  //0 is north, 1 is south, 2 is east
 			direction = 'N';
 		else if (val > 15 && val <= 30)
@@ -136,6 +148,7 @@ int *motion(GridBox gridArray[6][5], char direction, int robotLocation) {
 	case 'S':
 		actualDirection[0] = 15; actualDirection[1] = 30; actualDirection[2] = 100;
 		val = randomNumberGen(100);
+		directionInt = 4;
 		if (val <= 15) //0 is west, 1 is east, 2 is south
 			direction = 'W';
 		else if (val > 15 && val <= 30)
@@ -144,51 +157,77 @@ int *motion(GridBox gridArray[6][5], char direction, int robotLocation) {
 			direction = 'S';
 		break;
 	}
-	int reward, directionInt;
+	int reward = 0;
 	double probability[4];
-	int robotPos[2];
+	int* robotPos;
+	robotPos = findLocation(state);
+	int i = robotPos[0];
+	int j = robotPos[1];
+	cout << robotPos[0] << "   " << robotPos[1];
 	//This switch statement performs the actual movement
 	switch (direction) {
 		//Translate robot location to row and column of grid
-		robotPos[0] = (robotLocation / 6); //row
-		robotPos[1] = (robotLocation % 5) - 1; //column; we subtract 1 so that it translates into an array value nicely
 
 	case 'W':
-		gridArray[robotPos[0]][robotPos[1]].accessFrequency[0] += 1;
-		reward = -2;
-		directionInt = 1;
-		//TODO: Implement movement, and possible bouncing.
+		gridArray[i][j].accessFrequency[0] += 1;
+		reward = -2; //for tuple
+		if (gridArray[i][j - 1].isObstacle == 0) {
+			state -= 1; //moving the robot
+		}
+		nextAction = findAction(gridArray, state);
 		break;
 	case 'N':
-		gridArray[robotPos[0]][robotPos[1]].accessFrequency[1] += 1;
+		gridArray[i][j].accessFrequency[1] += 1;
 		reward = -3;
-		directionInt = 2;
+		if (gridArray[i - 1][j].isObstacle == 0) {
+			state -= 5; //moving the robot
+		}
+		nextAction = findAction(gridArray, state);
 		break;
 	case 'E':
-		gridArray[robotPos[0]][robotPos[1]].accessFrequency[2] += 1;
+		gridArray[i][j].accessFrequency[2] += 1;
 		reward = -2;
-		directionInt = 3;
+		if (gridArray[i][j + 1].isObstacle == 0) {
+			state += 1; //moving the robot
+		}
+		nextAction = findAction(gridArray, state);
 		break;
 	case 'S':
-		gridArray[robotPos[0]][robotPos[1]].accessFrequency[3] += 1;
+		gridArray[i][j].accessFrequency[3] += 1;
 		reward = -1;
-		directionInt = 4;
+		if (gridArray[i + 1][j].isObstacle == 0) {
+			state += 5; //moving the robot
+		}
+		nextAction = findAction(gridArray, state);
 		break;
 	}
-	for (int i = 0; i < 6; i++) {
-		for (int j = 0; j < 5; j++) {
-			gridArray[i][j].probability = gridArray[i][j].transProbability;
-		}
+	switch (nextAction) {
+	case 'W':
+		nextActionInt = 1;
+		break;
+	case 'N':
+		nextActionInt = 2;
+		break;
+	case 'E':
+		nextActionInt = 3;
+		break;
+	case 'S':
+		nextActionInt = 4;
+		break;
 	}
-		int tuple[2];
-		tuple[0] = directionInt;
-		tuple[1] = reward;
+		tuple[1] = directionInt; //current action in tuple
+		tuple[2] = reward; //reward in tuple
+		tuple[3] = state; //next state in tuple
+		tuple[4] = nextActionInt; //next action in tuple
+		//so by this time we have a tuple with current state, current action, reward, next state, and next action. This gets us ready for Q-learning
 	return tuple;
 }
 
-void trial(Robot robot, GridBox gridArray[6][5], int moves = 0) {
+void trial(int robotLocation, GridBox gridArray[6][5], int moves = 0) {
 	bool obstacleFound = 1;
 	int startingSquare;
+	char direction;
+	int *tuple;
 	if (moves == 0) { //we have to spawn the robot
 		while (obstacleFound) {
 			startingSquare = randomNumberGen(30);
@@ -203,21 +242,29 @@ void trial(Robot robot, GridBox gridArray[6][5], int moves = 0) {
 				obstacleFound = 0;
 			}
 		}
-
-		robot.location = startingSquare;
+		moves = 1;
+		robotLocation = startingSquare;
 	}
-	//TODO: Add function for choosing optimal policy vs random action and then motion based on the action
-
-	if (moves != 100) {//base case
-		trial(robot, gridArray, ++moves);
+	direction = findAction(gridArray, robotLocation);
+	//TODO: set a tuple variable to the result of motion so we can update the q-values
+	tuple = motion(gridArray, direction, robotLocation);
+	//TODO: function that updates q-values
+	if (moves != 101) {//base case
+		trial(robotLocation, gridArray, ++moves);
 	}
 }
 
+/*
+	findAction
+	purpose: to determine whether the robot will follow the optimal policy or choose a random action
+		gridArray: the grid that determines the qValues for the optimal policy
+		robotLocation: the robot's location in integer value
+*/
 char findAction(GridBox gridArray[6][5], int robotLocation) {
 	int value = randomNumberGen(100);
 	char direction;
 	int *robotPos;
-	if (value <= 5) {
+	if (value <= 5) { //this is our implementation of epsilon-greedy reinforcement learning. 5% chance the robot chooses a random action rather than the optimal policy
 		value = randomNumberGen(4);
 		switch (value) {
 			case 1: //choose west
@@ -235,14 +282,71 @@ char findAction(GridBox gridArray[6][5], int robotLocation) {
 		}
 	}
 	else {
-		robotPos = findRobotLocation(robotLocation);
+		robotPos = findLocation(robotLocation);
+		int i = robotPos[0];
+		int j = robotPos[1];
+		int index;
+		float maxQValue = max(gridArray[i][j].qValue);
+		for (int i = 0; i < 4; i++) {
+			if (maxQValue == gridArray[i][j].qValue[i]) {
+				index = i;
+				break;
+			}
+		}
+		switch (index) {
+			case 0:
+				direction = 'W';
+				break;
+			case 1:
+				direction = 'N';
+				break;
+			case 2:
+				direction = 'E';
+				break;
+			case 3:
+				direction = 'S';
+				break;
+		}
 	}
 	return direction;
 }
 
-int* findRobotLocation(int robotLocation) {
-	int robotPos[2];
-	robotPos[0] = (robotLocation / 6); //row
-	robotPos[1] = (robotLocation % 5) - 1; //column
-	return robotPos;
+int* findLocation(int location) {
+	static int position[2];
+	if (location % 5 != 0) {
+		position[0] = (location / 5); //row
+		position[1] = (location % 5) - 1; //column
+	}
+	else {
+		position[0] = (location / 5) - 1;
+		position[1] = 4;
+	}
+	
+	return position;
+}
+
+/*
+	updateQValues
+	purpose: updates the q values of the box in the grid after the robot takes its action
+		gridArray: grid that contains boxes the robot traverses
+		tuple: contains information for updating q-values; 0-current state, 1-current action, 2-reward, 3-next state, 4-next action
+*/
+void updateQValues(GridBox gridArray[6][5], int tuple[5]) {
+	int *currentState = findLocation(tuple[0]); //finds robot's current location
+	int currentAction = tuple[1];
+	int reward = tuple[2];
+	int* nextState = findLocation(tuple[3]);
+	int nextAction = tuple[4];
+	int i = currentState[0]; int j = currentState[1];
+	gridArray[i][j].qValue[currentAction - 1] = (1 / gridArray[i][j].accessFrequency[currentAction - 1]) * (reward + .9 * max(nextState) - gridArray[i][j].qValue[currentAction - 1]);
+
+}
+
+float max(int array[]) {
+	float max = array[0];
+	for (int i = 1; i < sizeof(array); i++) {
+		if (array[i] > max)
+			max = array[i];
+	}
+	return max;
 }
